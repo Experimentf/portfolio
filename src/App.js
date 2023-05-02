ac3db00736cb42b4db7185c7ae74c14b7d496805
Divyansh Falodiya <divyanshfalodiyaab@gmail.com>
2023-05-02 21:08:57 +0530

    fix: removed old assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import { CameraControls } from "@react-three/drei";
-import Room from "./components/Room";
+import logo from './logo.svg';
+import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { CameraControls } from '@react-three/drei';
 
 function App() {
-  return (
-    <Canvas
-      style={{
-        width: "100vw",
-        height: "100vh",
-        background: "#171717",
-      }}
-    >
-      <ambientLight />
-      <CameraControls />
-      <Room />
-    </Canvas>
-  );
+    return (
+        <Canvas
+            style={{
+                width: '100vw',
+                height: '100vh',
+                background: '#171717',
+            }}
+        >
+            <ambientLight />
+            <CameraControls />
+        </Canvas>
+    );
 }
 
 export default App;
